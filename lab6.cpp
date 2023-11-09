9aee8a377c961dc60f4c1d9433770f472fa14189
tolyaSIKE <blachhawcks@gmail.com>
2023-11-09 09:38:52 +0200

    sending file to repository

---
+++ lab6.cpp
@@ -0,0 +1,27 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	double a, b, h, y, x;
+	cout << "Enter [a; b], h" << endl;
+	cin >> a >> b >> h;
+	x = a;
+	/* while (x <= b + h / 2)
+	{
+		y = sin(x) * fabs (x) + pow (2,x);
+		cout << '|' << setw(6) << x << "\t|\t" << setw(10) << y << '|' << endl;
+		x += h;
+	}*/
+	do {
+		y = sin(x) * fabs(x) + pow(2, x);
+		cout << '|' << setw(6) << x << "\t|\t" << setw(10) << y << '|' << endl;
+		x += h;
+	} while (x <= b + h / 2);
+}
