0ea4e27730d134f275a3915fa712bc9851a117f9
tolyaSIKE <blachhawcks@gmail.com>
2023-11-23 04:20:42 +0200

    add new file

---
+++ 9lab(1).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	// Варіант номер 1
+	srand(time(NULL));
+	const int n = 10;
+	int mas[n], sum=0, max, maxInd;
+	for (int i = 0; i < n; i++) {
+		mas[i] = rand() % 41 - 20;
+		cout << "mas[" << i << "]=" << mas[i] << endl;
+	}
+	cout << "++++++++++++++++++++++++++\n";
+	for (int i = 0; i < n; i++) {
+		if (mas[i]>0) {
+			sum = sum + mas[i];	
+		}
+	}
+	cout << sum << " " << endl;
+	cout << "++++++++++++++++++++++++++\n";
+	max = mas[n];
+	maxInd = 0;
+	for (int i = 0; i < n; i++) {
+		if (mas[i] > max) {
+			max = mas[i];
+			maxInd = i;
+		}
+	}
+	cout << "max=" << max << endl << "maxInd=" << maxInd;
+}
